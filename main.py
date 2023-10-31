dd210bfa694dc1fc950de0f6bef1a8ab94a78352
Ilicheva <nastya.ilicheva.2006@gmail.com>
2023-10-31 17:49:11 +0400

    start2

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 import sys
-
-# Импортируем из PyQt5.QtWidgets классы для создания приложения и виджета
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QInputDialog, QPushButton, QTextEdit
 
 
-# Унаследуем наш класс от простейшего графического примитива QWidget
 class Example(QWidget):
     def __init__(self):
         # Надо не забыть вызвать инициализатор базового класса
@@ -12,12 +9,26 @@ class Example(QWidget):
         # В метод initUI() будем выносить всю настройку интерфейса,
         # чтобы не перегружать инициализатор
         self.initUI()
+        age, ok_pressed = QInputDialog.getInt(
+            self, "Введите возраст", "Сколько тебе лет?",
+            1, 1, 8, 1)
 
     def initUI(self):
-        # Зададим размер и положение нашего виджета,
         self.setGeometry(300, 300, 300, 300)
-        # А также его заголовок
-        self.setWindowTitle('Первая программа')
+        self.setWindowTitle('Eldrich Horror')
+
+
+
+        # self.button_1 = QPushButton(self)
+        # self.button_1.move(20, 40)
+    #     self.button_1.setText("Кнопка")
+    #     self.button_1.clicked.connect(self.run)
+    #
+    # def run(self):
+    #     name, ok_pressed = QInputDialog.getText(self, "Введите имя",
+    #                                             "Как тебя зовут?")
+    #     if ok_pressed:
+    #         self.button_1.setText(name)
 
 
 if __name__ == '__main__':
